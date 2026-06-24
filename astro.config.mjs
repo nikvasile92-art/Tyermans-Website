f458b90bc858307dc5a2366a5a1d4115f97f4024
Kevin Knopp <kknopper@gmail.com>
2026-06-24 13:50:36 -0500

    setup sitemap for google, added robots.txt and llms.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://tyermans.com',
+  integrations: [react(), sitemap()],
   image: {
     quality: 80,
+  },
+  build: {
+    inlineStylesheets: 'always',
   },
 });
